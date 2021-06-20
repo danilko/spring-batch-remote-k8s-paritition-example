8b2d1c9c768badfaa878029a593f8bca1f65f748
Danil Ko <danilko@localhost.localdomain>
2021-06-19 23:23:04 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,113 @@
 # spring-batch-remote-k8s-paritition-example
 Example to setup Spring Batch with Remote Partition Execution through Spring Cloud Deployer Kuberentes 
+Derived from Spring IO default local deployer, and update to make to deploy on K8S (also make it compile and work on my local laptop, as original sample seem not work out of box for me)
+
+Reference
+
+- [Spring Cloud Deployer Kuberentes, discover method through test code] https://github.com/spring-cloud/spring-cloud-deployer-kubernetes/
+- [Spring Cloud Tutorial on Remote Deployment, though the default code seem not able to compile for me] https://dataflow.spring.io/docs/feature-guides/batch/partitioning/
+
+Assume have local kuberentes and controller will be run on local with worker deploy to kuberentes
+
+Build the image
+```aidl 
+mvn clean package
+docker build . -t worker
+```
+
+Setup a MariaDB
+```
+kubectl apply -f mariadb.deployment.yaml
+```
+
+Expose the DB for local controller
+```
+kubectl port-forward deployment/mariadb 3306:3306
+```
+
+On different terminal, start the jar
+```
+export SPRING_DATASOURCE_PASSWORD="password"
+export SPRING_DATASOURCE_USERNAME="spring"
+export SPRING_DATASOURCE_URL="jdbc:mysql://localhost:3306/spring"
+export SPRING_DATASOURCE_DRIVERCLASSNAME="org.mariadb.jdbc.Driver"
+export SPRING_PROFILES_ACTIVE="controller"
+export SPRING_BATCH_INITIALIZE_SCHEMA=always
+
+java -jar target/batchprocessing-0.0.1-SNAPSHOT.jar
+```
+
+If quick enough, will able to see following, where the pod is spawned up as part of remote partition
+```aidl
+kubectl get pods
+NAME                                 READY   STATUS      RESTARTS   AGE
+mariadb-6b48f78bbf-bmxsm             1/1     Running     0          4h7m
+partitionedbatchjobtask-6zjr0mg7ny   1/1     Running     0          2s         11m
+partitionedbatchjobtask-kdgp9w0exk   1/1     Running     0          2s
+```
+
+Each pod will be carry its own partition context (partition0, partition1)
+Can be seen inside the pod definition on args field
+```aidl
+    Args:
+      --spring.profiles.active=worker
+      --spring.cloud.task.initialize.enable=false
+      --spring.batch.initializer.enabled=false
+      --spring.cloud.task.job-execution-id=34
+      --spring.cloud.task.step-execution-id=104
+      --spring.cloud.task.step-name=workerStep1
+      --spring.cloud.task.name=application_partitionedJob1532999942_workerStep1:partition1
+      --spring.cloud.task.parentExecutionId=69
+      --spring.cloud.task.executionid=71
+
+```
+
+The pod current clean up is done through brute force, as this one example is not using full spring cloud method
+In after job, force clean up success/failed pod, if need, can comment out the deletion line to let pod remain
+```aidl
+  @Bean
+    public JobExecutionListener jobExecutionListener() {
+    JobExecutionListener listener = new JobExecutionListener(){
+        @Override
+        public void beforeJob(JobExecution JobExecution)
+        {
+            // Auto generated method
+        }
+
+        @Override
+        public void afterJob(JobExecution jobExecution) {
+            // Clean up jobs
+            Map<String, String> fields = new HashMap<String, String>();
+            fields.put("status.phase","Succeeded");
+
+            Map<String, String> labels = new HashMap<String, String>();
+            labels.put("role","spring-app");
+
+            kuberentesClient().pods().inNamespace("default").withLabels(labels).withFields(fields).delete();
+
+            fields.put("status.phase","Failed");
+            kuberentesClient().pods().inNamespace("default").withLabels(labels).withFields(fields).delete();
+
+        }
+    };
+```
+
+Also demo how to use DockerResource and TaskLauncher to inject custom remote docker image with pod computing
+
+Custom K8S pod setting for CPU/MEMORY can be found in
+```aidl
+    @Bean
+    public TaskLauncher taskLauncher()
+...
+
+Custom Docker Image
+```aidl
+    @Bean("partitionHandler")
+    public PartitionHandler partitionHandler(TaskLauncher taskLauncher,
+                                             JobExplorer jobExplorer) throws Exception {
+
+        // Use local build image
+        DockerResource resource = new DockerResource("worker:latest");
+...
+
+```
---
+++ src/main/java/com/example/batchprocessing/BatchConfiguration.java
@@ -0,0 +1,269 @@
+package com.example.batchprocessing;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.partition.PartitionHandler;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.deployer.resource.docker.DockerResource;
+import org.springframework.cloud.deployer.resource.support.DelegatingResourceLoader;
+import org.springframework.cloud.deployer.spi.kubernetes.*;
+import org.springframework.cloud.deployer.spi.task.TaskLauncher;
+import org.springframework.cloud.task.batch.partition.*;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.cloud.task.repository.TaskRepository;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.core.task.TaskRejectedException;
+
+import java.util.*;
+
+
+@Configuration
+@EnableBatchProcessing
+@EnableTask
+public class BatchConfiguration {
+
+    // Set the grid (also controller worker)
+    private static final int  GRID_SIZE = 2;
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public JobExplorer jobExplorer;
+
+    @Autowired
+    public JobRepository jobRepository;
+
+    @Autowired
+    public TaskExecutor taskExecutor;
+
+    @Autowired
+    public TaskRepository taskRepository;
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @Autowired
+    private DelegatingResourceLoader resourceLoader;
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public Partitioner partitioner() {
+        return new Partitioner() {
+            @Override
+            public Map<String, ExecutionContext> partition(int gridSize) {
+
+                Map<String, ExecutionContext> partitions = new HashMap<>(gridSize);
+
+                for (int i = 0; i < GRID_SIZE; i++) {
+                    ExecutionContext context1 = new ExecutionContext();
+                    context1.put("partitionNumber", i);
+
+                    partitions.put("partition" + i, context1);
+                }
+
+                return partitions;
+            }
+        };
+    }
+
+    @Bean
+    public KubernetesClient kuberentesClient()
+    {
+        KubernetesDeployerProperties kubernetesDeployerProperties = new KubernetesDeployerProperties();
+
+        return KubernetesClientFactory.getKubernetesClient(kubernetesDeployerProperties);
+    }
+
+    @Bean
+    public TaskLauncher taskLauncher()
+    {
+        KubernetesDeployerProperties kubernetesDeployerProperties = new KubernetesDeployerProperties();
+        kubernetesDeployerProperties.setNamespace("default");
+
+        //Hard to clean up so not using jobs for now
+        //kubernetesDeployerProperties.setCreateJob(true);
+
+        // Database setup to reference configmap for database info
+        List<KubernetesDeployerProperties.ConfigMapKeyRef> configMapKeyRefList = new ArrayList<KubernetesDeployerProperties.ConfigMapKeyRef>();
+        KubernetesDeployerProperties.ConfigMapKeyRef configMapKeyRef = new KubernetesDeployerProperties.ConfigMapKeyRef();
+        configMapKeyRef.setConfigMapName("mariadb");
+        configMapKeyRef.setDataKey("SPRING_DATASOURCE_URL");
+        configMapKeyRef.setEnvVarName("SPRING_DATASOURCE_URL");
+        configMapKeyRefList.add(configMapKeyRef);
+
+        configMapKeyRef = new KubernetesDeployerProperties.ConfigMapKeyRef();
+        configMapKeyRef.setConfigMapName("mariadb");
+        configMapKeyRef.setDataKey("SPRING_DATASOURCE_USERNAME");
+        configMapKeyRef.setEnvVarName("SPRING_DATASOURCE_USERNAME");
+        configMapKeyRefList.add(configMapKeyRef);
+
+        configMapKeyRef = new KubernetesDeployerProperties.ConfigMapKeyRef();
+        configMapKeyRef.setConfigMapName("mariadb");
+        configMapKeyRef.setDataKey("SPRING_DATASOURCE_PASSWORD");
+        configMapKeyRef.setEnvVarName("SPRING_DATASOURCE_PASSWORD");
+        configMapKeyRefList.add(configMapKeyRef);
+
+        configMapKeyRef = new KubernetesDeployerProperties.ConfigMapKeyRef();
+        configMapKeyRef.setConfigMapName("mariadb");
+        configMapKeyRef.setDataKey("SPRING_DATASOURCE_DRIVERCLASSNAME");
+        configMapKeyRef.setEnvVarName("SPRING_DATASOURCE_DRIVERCLASSNAME");
+        configMapKeyRefList.add(configMapKeyRef);
+
+        configMapKeyRef = new KubernetesDeployerProperties.ConfigMapKeyRef();
+        configMapKeyRef.setConfigMapName("mariadb");
+        configMapKeyRef.setDataKey("SPRING_PROFILES_ACTIVE");
+        configMapKeyRef.setEnvVarName("SPRING_PROFILES_ACTIVE");
+        configMapKeyRefList.add(configMapKeyRef);
+
+        kubernetesDeployerProperties.setConfigMapKeyRefs(configMapKeyRefList);
+
+        // Set request resource
+        KubernetesDeployerProperties.RequestsResources request = new KubernetesDeployerProperties.RequestsResources();
+        request.setCpu("2");
+        request.setMemory("2000Mi");
+
+        KubernetesDeployerProperties.LimitsResources limit = new KubernetesDeployerProperties.LimitsResources();
+        limit.setCpu("3");
+        limit.setMemory("3000Mi");
+
+        kubernetesDeployerProperties.setRequests(request);
+        kubernetesDeployerProperties.setLimits(limit);
+
+        // as build on local image, so need to use local
+        kubernetesDeployerProperties.setImagePullPolicy(ImagePullPolicy.IfNotPresent);
+
+        // Set task launcher properties to not repeat and not restart
+        KubernetesTaskLauncherProperties kubernetesTaskLauncherProperties = new KubernetesTaskLauncherProperties();
+        kubernetesTaskLauncherProperties.setBackoffLimit(1);
+        kubernetesTaskLauncherProperties.setRestartPolicy(RestartPolicy.Never);
+        KubernetesTaskLauncher kubernetesTaskLauncher = new KubernetesTaskLauncher(kubernetesDeployerProperties,
+                kubernetesTaskLauncherProperties, kuberentesClient());
+
+        return kubernetesTaskLauncher;
+    }
+
+    @Bean(name = "partitionedJob")
+    @Profile("!worker")
+    public Job partitionedJob(PartitionHandler partitionHandler)throws Exception {
+        Random random = new Random();
+        return jobBuilderFactory.get("partitionedJob" + random.nextInt())
+                .start(partitionStep1(partitionHandler))
+                .listener(jobExecutionListener())
+                .build();
+    }
+
+    @Bean(name = "partitionStep1")
+    public Step partitionStep1(PartitionHandler partitionHandler) throws Exception {
+        return stepBuilderFactory.get("partitionStep1")
+                .partitioner(workerStep1().getName(), partitioner())
+                .step(workerStep1())
+                .partitionHandler(partitionHandler)
+                .build();
+    }
+
+    @Bean("partitionHandler")
+    public PartitionHandler partitionHandler(TaskLauncher taskLauncher,
+                                             JobExplorer jobExplorer) throws Exception {
+
+        // Use local build image
+        DockerResource resource = new DockerResource("worker:latest");
+
+        DeployerPartitionHandler partitionHandler =
+                new DeployerPartitionHandler(taskLauncher, jobExplorer, resource, "workerStep1", taskRepository);
+
+        List<String> commandLineArgs = new ArrayList<>(3);
+        commandLineArgs.add("--spring.profiles.active=worker");
+        commandLineArgs.add("--spring.cloud.task.initialize.enable=false");
+        commandLineArgs.add("--spring.batch.initializer.enabled=false");
+        partitionHandler
+                .setCommandLineArgsProvider(new PassThroughCommandLineArgsProvider(commandLineArgs));
+        partitionHandler.setEnvironmentVariablesProvider(new NoOpEnvironmentVariablesProvider());
+        partitionHandler.setMaxWorkers(GRID_SIZE);
+        partitionHandler.setApplicationName("PartitionedBatchJobTask");
+
+        return partitionHandler;
+    }
+
+    @Bean
+    public JobExecutionListener jobExecutionListener() {
+    JobExecutionListener listener = new JobExecutionListener(){
+        @Override
+        public void beforeJob(JobExecution JobExecution)
+        {
+            // Auto generated method
+        }
+
+        @Override
+        public void afterJob(JobExecution jobExecution) {
+            // Clean up jobs
+            Map<String, String> fields = new HashMap<String, String>();
+            fields.put("status.phase","Succeeded");
+
+            Map<String, String> labels = new HashMap<String, String>();
+            labels.put("role","spring-app");
+
+            kuberentesClient().pods().inNamespace("default").withLabels(labels).withFields(fields).delete();
+
+            fields.put("status.phase","Failed");
+            kuberentesClient().pods().inNamespace("default").withLabels(labels).withFields(fields).delete();
+
+        }
+    };
+
+    return listener;
+    }
+
+    @Bean
+    @Profile("worker")
+    public DeployerStepExecutionHandler stepExecutionHandler(JobExplorer jobExplorer) {
+        return new DeployerStepExecutionHandler(this.context, jobExplorer, this.jobRepository);
+    }
+
+    @Bean(name = "workerStep1")
+    public Step workerStep1() {
+        return this.stepBuilderFactory.get("workerStep1")
+                .tasklet(workerTasklet(null))
+                .build();
+    }
+
+
+    @Bean
+    @StepScope
+    public Tasklet workerTasklet(
+            final @Value("#{stepExecutionContext['partitionNumber']}") Integer partitionNumber) {
+
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This tasklet ran partition: " + partitionNumber);
+
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
